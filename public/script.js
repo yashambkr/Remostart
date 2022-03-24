1f7b35e1c3ec2d51de7d31bb0c6fd24c3bb18b34
Aniketsomkuwar <aniketsomkuwar1101@gmail.com>
2022-03-24 19:09:17 +0530

    fixed signup page

--- public/script.js
+++ public/script.js
@@ -1,42 +1,21 @@
 /*===== LOGIN SHOW and HIDDEN =====*/
 
-const signUp = document.getElementById("sign-up"),
-  signIn = document.getElementById("sign-in"),
-  loginIn = document.getElementById("login-in"),
+const loginIn = document.getElementById("login-in"),
   loginUp = document.getElementById("login-up"),
   forgetpassword = document.getElementById("forgot-button"),
   forgot = document.getElementById("forget-password");
 
-signUp.addEventListener("click", () => {
-  // Remove classes first if they exist
-  loginIn.classList.remove("block");
-  loginUp.classList.remove("none");
-
-  // Add classes
-  loginIn.classList.toggle("none");
-  loginUp.classList.toggle("block");
-});
-
-signIn.addEventListener("click", () => {
-  // Remove classes first if they exist
-  loginIn.classList.remove("none");
-  loginUp.classList.remove("block");
-
-  // Add classes
-  loginIn.classList.toggle("block");
-  loginUp.classList.toggle("none");
-});
 
 forgetpassword.addEventListener("click", () => {
   // Remove classes first if they exist
-  loginIn.classList.remove("none");
+  loginIn.classList.remove("none-forgot");
   loginIn.classList.remove("block");
   forgot.classList.toggle("block");
 
   // Add classes
   loginIn.classList.toggle("block");
-  loginIn.classList.toggle("none");
-  forgot.classList.toggle("none");
+  loginIn.classList.toggle("none-forgot");
+  forgot.classList.toggle("none-forgot");
 });
 // end of switiching forms
 
